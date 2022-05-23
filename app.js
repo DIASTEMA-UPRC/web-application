d3e3db41a30a4cf024e3fc1da6c1e55516b8aa1f
adreaskar <adrian26mccous@gmail.com>
2022-05-23 15:09:31 +0300

    added missing endpoints

--- app.js
+++ app.js
@@ -66,6 +66,8 @@ app.route("/")
 
                     req.session.user = req.body.username;
                     req.session.organization = data[0].organization;
+                    req.session.property = data[0].property;
+                    req.session.email = data[0].email;
 
                     res.redirect("/dashboard");
                 }
@@ -103,15 +105,101 @@ app.route("/logout")
         });
     });
 
+// Dashboard route -----------------------------------------------------------------------------------------------------------
 app.route("/dashboard")
     .get((req,res) => {
-        res.render("dashboard");
+
+        const username = req.session.user;
+        const organization = req.session.organization;
+        const property = req.session.property;
+
+        res.render("dashboard", {user:username, org:organization, prop:property});
     })
 
+// Data Ingestion route ------------------------------------------------------------------------------------------------------
+app.route("/ingestion")
+    .get((req,res) => {
+
+        const username = req.session.user;
+        const organization = req.session.organization;
+        const property = req.session.property;
+
+        res.render("ingestion", {user:username, org:organization, prop:property});
+
+    })
+    .post((req,res) => {
+
+        // Generate ingestion id
+        const id = Math.random().toString(16).slice(2);
+
+        // Create datetime
+        const d = new Date();
+        const date = ('0'+d.getDate()).slice(-2) + "-" + ('0'+(d.getMonth()+1)).slice(-2) + "-" + d.getFullYear();
+        const time = ('0'+d.getHours()).slice(-2) + ":" + ('0'+d.getMinutes()).slice(-2) + ":" + ('0'+d.getSeconds()).slice(-2) + ":" + d.getMilliseconds()
+
+        const datetime = date + " " + time;
+
+        // Data structure 
+        const data = {
+            "ingestion-datetime": datetime,
+            "diastema-token": "diastema-key",
+            "ingestion-id": id.toLowerCase(),
+            "database-id": req.session.organization.toLowerCase(),
+            "method": req.body.method,
+            "link": req.body.link,
+            "token": req.body.token,
+            "dataset-label": req.body.label.toLowerCase(),
+            "user": req.session.user
+        }
+
+        // Save data to MongoDB
+        const dataset = new Dataset ({
+            label: data["dataset-label"],
+            ingestationId: data["ingestion-id"],
+            ingestionDateTime: data["ingestion-datetime"],
+            organization: data["database-id"],
+            user: data.user,
+            method: data.method,
+            link: data.link,
+            token: data.token
+        });
+        dataset.save()
+        console.log("[INFO] Dataset information saved to MongoDB!");
+
+        // Send data to Orchestrator
+        fetch(ORCHESTRATOR_INGESTION_URL, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        }).then(res => {
+            console.log("[INFO] Ingestion data sent to orchestrator!", res);
+        });
+
+        res.redirect("/ingestion");
+    });
+
+// Pipelines route -----------------------------------------------------------------------------------------------------------
 app.route("/pipelines")
 .get((req,res) => {
-    res.render("pipelines");
+
+    const username = req.session.user;
+    const organization = req.session.organization;
+    const property = req.session.property;
+
+    res.render("pipelines", {user:username, org:organization, prop:property});
 })
+
+// Modelling route -----------------------------------------------------------------------------------------------------------
+app.route("/modelling")
+.get((req,res) => {
+
+    //req.session.analysisid = Math.random().toString(16).slice(2);
+
+    const username = req.session.user;
+    const id = Math.random().toString(16).slice(2);
+
+    res.render("modelling", {user:username, id:id});
+});
 
 server.listen(PORT, function () {
     console.log('Started on port 5400');
