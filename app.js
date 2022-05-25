1f779d6bbd0d97b7fd97a481e39acf2c640e361b
adreaskar <adrian26mccous@gmail.com>
2022-05-25 14:04:27 +0300

    updated login/register endpoints and added missing endpoints

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const MongoStore = require('connect-mongo');
 const mongoose = require("mongoose");
 let crypto = require('node:crypto');
 const http = require("http");
-const fs = require('fs');
 const session = require('express-session');
-const upload = require('express-fileupload');
+const multer = require('multer');
+const path = require('path');
 const fetch = require('node-fetch');
 const { MONGO_URL, PORT, ORCHESTRATOR_URL, ORCHESTRATOR_INGESTION_URL } = require("./config/config");
 
@@ -28,13 +28,21 @@ const Dataset = mongoose.main.model("Dataset", datasetSchema, "datasets");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({storage:storage});
 
 // Middleware ----------------
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json()); 
 app.use(express.static("public"));
-app.use(upload());
 app.use(session({
     secret:'diastema',
     resave:false,
@@ -52,25 +60,28 @@ app.route("/")
         if (req.session.user) {
             res.redirect("/dashboard");
         } else {
-            res.render("login");
+            res.render("login", {flag:false});
         }
     })
     .post((req,res) => {
         req.body.password = crypto.createHash('sha256').update(req.body.password).digest('hex');
         
+        // Search for existing user with given username and password ----------------------
         User.find({username:req.body.username, password:req.body.password}, (err,data) => {
             if (err) {
                 console.log(err);
             } else {
+                
                 if (data.length === 0) {
-                    console.log('[INFO] No user found');
-                    res.redirect("/");
+                    console.log('[ERROR] Login failed: No user found');
+                    res.render("login", {flag:true});
                 } else {
 
                     req.session.user = req.body.username;
                     req.session.organization = data[0].organization;
                     req.session.property = data[0].property;
                     req.session.email = data[0].email;
+                    req.session.image = data[0].image;
 
                     res.redirect("/dashboard");
                 }
@@ -81,37 +92,45 @@ app.route("/")
 // Register route -----------------------------------------------------------------------------------------------------------
 app.route("/register")
     .get((req,res) => {
-        res.render("register");
+        res.render("register", {flag:false});
     })
-    .post((req,res) => {
-
-        console.log(req.files);
-
-        const uploadpath = 'public/img/user-profiles/';
-
-        // Save file to local storage
-        req.files.inpFile.mv(uploadpath + req.files.inpFile.name, function(error){
-            if (error) {
-                console.log("[ERROR] Could not copy image to local storage.");
-                console.log(error);
-            } else {
-                console.log("[INFO] Image uploaded successfully.");
-            }
-        });
+    .post(upload.single("inpFile"), (req,res) => {
 
         req.body.password = crypto.createHash('sha256').update(req.body.password).digest('hex');
 
-        const user = new User ({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            organization: req.body.organization,
-            property: req.body.property
+        // Search for existing users with given username and email ------------------
+        User.find({username:req.body.username, email:req.body.email}, (err,data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                
+                if (data.length === 0) {
+                    console.log('[INFO] Register success: No user found');
+
+                    let image = 'default.png';
+                    if (req.file) {
+                       image = req.file.filename;
+                    }
+
+                    const user = new User ({
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: req.body.password,
+                        organization: req.body.organization,
+                        property: req.body.property,
+                        image:image
+                    });
+            
+                    user.save();
+
+                    res.redirect("/");
+                } else {
+
+                    console.log('[ERROR] Register failed: User exists');
+                    res.render("register", {flag:true});
+                }
+            }
         });
-
-        user.save();
-
-        res.redirect("/")
     });
 
 // Logout route -------------------------------------------------------------------------------------------------------------
@@ -130,8 +149,9 @@ app.route("/dashboard")
         const username = req.session.user;
         const organization = req.session.organization;
         const property = req.session.property;
+        const image = req.session.image;
 
-        res.render("dashboard", {user:username, org:organization, prop:property});
+        res.render("dashboard", {user:username, org:organization, prop:property, img:image});
     })
 
 // Data Ingestion route ------------------------------------------------------------------------------------------------------
@@ -142,7 +162,7 @@ app.route("/ingestion")
         const organization = req.session.organization;
         const property = req.session.property;
 
-        res.render("ingestion", {user:username, org:organization, prop:property});
+        res.render("ingestion", {user:username, org:organization, prop:property, img:image});
 
     })
     .post((req,res) => {
@@ -210,7 +230,7 @@ app.route("/pipelines")
     const organization = req.session.organization;
     const property = req.session.property;
 
-    res.render("pipelines", {user:username, org:organization, prop:property});
+    res.render("pipelines", {user:username, org:organization, prop:property, img:image});
 })
 
 // Modelling route -----------------------------------------------------------------------------------------------------------
@@ -242,6 +262,62 @@ app.route("/visualization")
 .get((req,res) => {
     res.render("404");
 })
+
+// Messaging route -----------------------------------------------------------------------------------------------------------
+app.route("/messages")
+    .post((req,res) => {
+        let data = req.body;
+
+        switch (data.message) {
+
+            case "update":
+                io.sockets.emit("Modeller", data.update);
+                res.sendStatus(200);
+                break;
+
+            case "send-to-orchestrator":
+                console.log("[INFO] Backend got the data");
+
+                fetch(ORCHESTRATOR_URL, {
+                    method: "POST",
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(data.info)
+                }).then(res => {
+                    console.log("[INFO] Graph data sent to orchestrator!", res);
+                });
+
+                res.sendStatus(200);
+                break;
+
+            case "save-graph":
+
+                const graph = new Graph ({
+                    user: data.info.metadata.user,
+                    name: data.info["analysis-name"],
+                    analysisid: data.info["analysis-id"],
+                    datetime: data.info["analysis-datetime"],
+                    jobs: data.info.jobs,
+                    nodes: data.info.nodes,
+                    connections: data.info.connections
+                });
+        
+                graph.save()
+
+                console.log("[INFO] Graph saved to MongoDB");
+
+                break;
+            
+            case "visualize":
+                io.sockets.emit("Modeller", "Data ready for visualization, visit the Dashboard");
+                res.sendStatus(200); 
+                break;
+
+            default:
+                console.log("[INFO] No data recieved");
+                res.sendStatus(500);
+                break;
+        }
+    });
 
 // 404 route -----------------------------------------------------------------------------------------------------------------
 app.route("/404")
