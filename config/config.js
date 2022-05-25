c875dcdccd4a7db457eaa469fdb75b3ae50e9b11
adreaskar <adrian26mccous@gmail.com>
2022-05-25 14:00:34 +0300

    updated PORT variable

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGO_URL: process.env.MONGO_URL || "mongo",
-    PORT: process.env.PORT || 6000,
+    PORT: process.env.PORT || 5400,
     ORCHESTRATOR_URL: process.env.ORCHESTRATOR_URL,
     ORCHESTRATOR_INGESTION_URL: process.env.ORCHESTRATOR_INGESTION_URL
 }
