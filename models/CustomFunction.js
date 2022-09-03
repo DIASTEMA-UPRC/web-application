e5f359e0855696b6d20b3276fde8209c1d4eb1f2
adreaskar <adrian26mccous@gmail.com>
2022-09-03 11:57:37 +0300

    added custom functions model

---
+++ models/CustomFunction.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const functionSchema = new mongoose.Schema ({
+    function_id: String,
+    name: String,
+    output_type: String,
+    color:String,
+    args: Object,
+    expressions: Object,
+    metadata: Object,
+    nodes: Object,
+    connections: Object
+});
+
+module.exports = mongoose.model("CustomFunction", functionSchema, "functions");
