3a507c7f2aa685e8b234ffe6b09ca6fe4b176fbe
adreaskar <adrian26mccous@gmail.com>
2022-10-10 19:05:15 +0300

    💎 style: Changed color to terminal logs

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const time = require('./public/js/modules/getTime.js');
 
 // chalk colors
 const diastema = chalk.hex('#3990b2');
-const error = chalk.white.bold.bgRed
-const success = chalk.white.bold.bgGreen
+const error = chalk.red.bold
+const info = chalk.yellow.bold
 
 const { MONGO_URL, PORT} = require("./config/config");
 
@@ -97,5 +97,5 @@ app.use(githubAuth);
 
 server.listen(PORT, function () {
     //console.log('[INFO] Started on port 5400');
-    console.log(success(time() + "[INFO]") + " Started on port 5400");
+    console.log(info(time()) + " Started on port 5400");
 });
