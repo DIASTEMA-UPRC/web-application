c93b23e2855e0012f8591073f80f02145d25e2f2
adreaskar <adrian26mccous@gmail.com>
2023-02-25 17:25:38 +0200

    🧹 cleanup: Cleanup of remaining changes

--- config/config.js
+++ config/config.js
@@ -1,12 +1,14 @@
 module.exports = {
+
+    HOST: process.env.HOST || "http://localhost",
+    PORT: process.env.PORT || 5400,
+
     MONGO_URL: process.env.MONGO_URL || "mongo",
 
     MINIO_URL: process.env.MINIO_URL,
     MINIO_PORT: 9000,
     MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY,
     MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY,
-
-    PORT: process.env.PORT || 5400,
 
     ORCHESTRATOR_URL: process.env.ORCHESTRATOR_URL,
     ORCHESTRATOR_INGESTION_URL: process.env.ORCHESTRATOR_INGESTION_URL,
